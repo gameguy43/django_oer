aea9b2708497b2931d464b20639a9227acfcf09a
Parker Phinney <parker.phinney@gmail.com>
2010-12-25 18:03:31 -0800

    AUTOCOMMIT: too lazy to write a msg

--- mysite/mainapp/models.py
+++ mysite/mainapp/models.py
@@ -8,12 +8,13 @@ class Tag(models.Model):
 
 class Thing(models.Model):
     title = models.CharField(max_length=300)
-    description = models.CharField(max_length=3000)
-    mini_desc = models.CharField(max_length=3000)
-    widget = models.CharField(max_length=10000)
+    description = models.TextField(max_length=3000)
+    mini_desc = models.TextField(max_length=3000)
+    widget = models.TextField(max_length=10000)
     big_image = models.CharField(max_length=10000)
+    karma = models.IntegerField(null=False)
+    hide = models.BooleanField(null=False)
     tags = models.ManyToManyField(Tag, verbose_name="list of tags")
-    karma = models.IntegerField()
     def __unicode__(self):
         return self.title
 
