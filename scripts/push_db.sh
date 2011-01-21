cbc006b9a613704050b0b24f40883cddee045e85
Parker Phinney <parker.phinney@gmail.com>
2011-01-21 03:08:01 -0500

    adjusted db push script so it actually works

--- scripts/push_db.sh
+++ scripts/push_db.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 # doesn't quite work yet--need to secretly but automatically pass the password. maybe through .my.cnf
 
-mysqldump -u ocw_dartmouth -p ocw_dartmouth > thedump.mysql && gzip thedump.mysql && scp thedump.mysql.gz parkcity:/tmp && ssh parkcity "zcat /tmp/thedump.mysql.gz | mysql -uocw_dartmouth -p ocw_dartmouth"
+mysqldump -u ocw_dartmouth -p ocw_dartmouth > thedump.mysql && gzip thedump.mysql && scp thedump.mysql.gz parkcity:/tmp && ssh -t parkcity "zcat /tmp/thedump.mysql.gz | mysql -uocw_dartmouth -p ocw_dartmouth"
 mv thedump.mysql.gz /tmp/
 
