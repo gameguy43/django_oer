9aaa5b2e05617788a1398c5266b83bb8cec48635
Parker Phinney <parker.phinney@gmail.com>
2011-01-24 17:45:54 -0500

    trying to make the pushing scripts more robust...

--- scripts/push_db.sh
+++ scripts/push_db.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 # doesn't quite work yet--need to secretly but automatically pass the password. maybe through .my.cnf
 
+MYSQL_PASS=`cat mysql_pass`
+echo $MYSQL_PASS
 mysqldump -u ocw_dartmouth -p ocw_dartmouth > thedump.mysql && gzip thedump.mysql && scp thedump.mysql.gz parkcity:/tmp && ssh -t parkcity "zcat /tmp/thedump.mysql.gz | mysql -uocw_dartmouth -p ocw_dartmouth"
+#mysqldump -u ocw_dartmouth -p ocw_dartmouth > thedump.mysql && gzip thedump.mysql && scp thedump.mysql.gz parkcity:/tmp && ssh -t parkcity "zcat /tmp/thedump.mysql.gz | mysql -uocw_dartmouth -p ocw_dartmouth"
 mv thedump.mysql.gz /tmp/
 
