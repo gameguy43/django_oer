007e169493c6ef32e991614a925a6959e5c721aa
Parker Phinney <parker.phinney@gmail.com>
2011-03-06 00:11:11 -0500

    added a few items

--- mysite/mainapp/views.py
+++ mysite/mainapp/views.py
@@ -15,7 +15,8 @@ import random
 
 def view_thing(request, thing__pk):
     thing = mysite.mainapp.models.Thing.objects.get(pk=thing__pk)
-    data = {'thing': thing}
+    total_items = mysite.mainapp.models.Thing.objects.all().count()
+    data = {'thing': thing, 'total_items': total_items}
     return render_to_response('view_thing.html', data)
     
 
@@ -24,13 +25,15 @@ def index(request):
     tags = mysite.mainapp.models.Tag.objects.filter(frontpageable=True)
     #things = mysite.mainapp.models.Thing.objects.order_by('karma').reverse()[0:5]
     #tags = mysite.mainapp.models.Tag.objects.all()[0:5]
-    data = {'things': things, 'tags': tags}
+    total_items = mysite.mainapp.models.Thing.objects.all().count()
+    data = {'things': things, 'tags': tags, 'total_items': total_items}
     return render_to_response('index.html', data)
 
 def everything(request):
     things = mysite.mainapp.models.Thing.objects.order_by('karma').reverse()
     tags = mysite.mainapp.models.Tag.objects.all()
-    data = {'things': things, 'tags':tags}
+    total_items = mysite.mainapp.models.Thing.objects.all().count()
+    data = {'things': things, 'tags':tags, 'total_items': total_items}
     return render_to_response('tag.html', data)
 
 def view_tag(request, tag__pk):
